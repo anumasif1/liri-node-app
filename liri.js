fd75bc7b9f9a40b1899949aa5da536c4171dd2bc
Anum Asif <unum.asif@gmail.com>
2019-07-30 00:04:05 -0700

    Liri App complete.

--- liri.js
+++ liri.js
@@ -3,11 +3,9 @@ require("dotenv").config();
 //Core node package for reading/writing files
 var fs = require("fs");
 var keys = require("./keys.js");
-//var log = require("./log.txt");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
-var inquirer = require("inquirer");
 var category = process.argv[2]
 var search = process.argv[3]
 var appendData = "";
@@ -24,8 +22,10 @@ var spotifyThisSong = function (songName) {
             var songName = ("Song:" + " " + (JSON.stringify(data.tracks.items[0].name)));
             var songAlbum = ("Album:" + " " + (JSON.stringify(data.tracks.items[0].album.name)));
             var songLink = ("Preview Link:" + " " + (JSON.stringify(data.tracks.items[0].preview_url)));
-            appendData = songArtist + "\n" + songName + "\n" + songAlbum + "\n" + songLink;
+            appendData = songArtist + "\n" + songName + "\n" + songAlbum + "\n" + songLink + "\n" + "\n";
             console.log(appendData);
+
+            //logging the result in log.txt file
             logFile();
         })
         .catch(function (err) {
@@ -33,10 +33,10 @@ var spotifyThisSong = function (songName) {
         });
 }
 
-//function for movie search
+//Movie-this function
 this.findMovie = function (movie) {
     var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
-    console.log(queryUrl);
+    // console.log(queryUrl);
 
     axios.get(queryUrl).then(
         function (response) {
@@ -49,7 +49,7 @@ this.findMovie = function (movie) {
             var movieLanguage = ("Language: " + response.data.Language);
             var moviePlot = ("Plot " + response.data.Plot);
             var movieActors = ("Actors: " + response.data.Actors);
-            appendData = movieTitle + "\n" + movieYear + "\n" + movieRating1 + "\n" + movieRating2 + "\n" + movieCountry + "\n" + movieLanguage + "\n" + moviePlot + "\n" + movieActors;
+            appendData = movieTitle + "\n" + movieYear + "\n" + movieRating1 + "\n" + movieRating2 + "\n" + movieCountry + "\n" + movieLanguage + "\n" + moviePlot + "\n" + movieActors + "\n" + "\n";
             console.log(appendData)
             logFile();
         })
@@ -62,7 +62,7 @@ this.findMovie = function (movie) {
         });
 }
 
-
+//Do-what-it-says Function
 var doWhatItSays = function () {
     fs.readFile("random.txt", "utf8", function (error, data) {
 
@@ -76,27 +76,29 @@ var doWhatItSays = function () {
 
         // Then split it by commas (to make it more readable)
         var dataArr = data.split(",");
-       // console.log(dataArr);
+
+        //run Spotify-this-song on the song in random.txt
         spotifyThisSong(dataArr[1]);
+        logFile();
 
     })
 }
 
 
-// if/else on the category songs/movie.
+// check category to run Spotify-this-song or Movie-this or Do-what-it-says
 if (category === "Spotify-this-song") {
     spotifyThisSong(search);
 } else if (category === "Movie-this") {
     this.findMovie(search);
-} else if (category === "Do-what-it-says"){
+} else if (category === "Do-what-it-says") {
     doWhatItSays();
 }
 
 
-// //log.txt => Append search results on log.txt file
-// // If the file didn't exist, then it gets created on the fly.
+// log.txt. Append search results on log.txt file
+// If the file didn't exist, then it gets created on the fly.
 var logFile = function () {
-    fs.appendFile("log.txt", appendData + "\n", "utf8", function (err) {
+    fs.appendFile("log.txt", appendData, "utf8", function (err) {
         if (err) {
             console.log("Error occurred: ", err);
         } else {
